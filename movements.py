3698b2dae80130823c1f1dfcfe302d17261640a8
Iker <freeiker@gmail.com>
2024-05-18 15:25:26 +0200

    new structure for the financeTracker in comment, not changes to code yet.

--- movements.py
+++ movements.py
@@ -16,17 +16,48 @@ class Movement:
     
 import pandas as pd
 
+#############################################################
+### ------------- CLASS FinanceTracker ------------------ ###
+### Class to contain movements as formatted data
+### The class main object is a dataframe wit the following columns:
+###     - Concepto: str
+###     - Categoria: str
+###     - Importe: float
+###     - Fecha: Date without time
+### The class should contain the following methods:
+###     - save_tracker: saves dataframe as json
+###     - load_tracker: loads dataframe from json
+###     - get_category_expenses: Return total amount for each category as a dataframe (index:category, column:total)
+###     - get_daily_expenses: Return total amount for each day as dataframe (index:Fecha (Date), colum:total)
+###             ·allow exclussion of certain categories (alquiler, salario, etc.)
+###     - add_movement: Adds a movement to the dataframe given: (concept, date, amount, category)
+###     - get_starting_date: returns the first (oldest) date in the dataframe: Date without time
+###     - get_end_date: returns the last (newest) date in the dataframe: Date without time
+### The class should overload the following methods:
+###     - __init__: initializes the dataframe
+###     - __add__: combines two dataframes, by adding the rows of one after the rows of the other
+###     - __str__: returns a string stating it is a FinanceTracker object ranging from {starting_date} to {end_date}.
+
 class FinanceTracker:
-    def __init__(self):
+    def __init__(self, name=None):
         self.category_map = {}  # Dictionary to store concept-category mappings
         self.categories = ["alquiler", "suscripciones", "ropa", "deporte", "sueldo", "beca", "comer fuera",
                            "supermercado", "transporte", "ocio", "salud", "ropa", "mascotas", "paga",
                            "regalos", "viajes", "bares", "fiesta", "internet", "tecnologia", "otros"]
+        # name of the Tracker if any
+        self.name = name
         # Convert categories to list and sort
         self.categories = sorted(self.categories)
-        # Initialize DataFrame with categories and set index to "category"
+        # Initialize DataFrame with categories to store the total expense per category
         self.total_expenses = pd.DataFrame(0.0, index=self.categories, columns=["total"])
+        # variable to store the final total expense
         self.total = 0.0
+    
+    def __str__(self):
+        return f"{self.name}"
+    
+    def set_name(self, name):
+        self.name = name
 
     def classify_concept(self, concept, category):
         self.category_map[concept] = category
@@ -57,5 +88,16 @@ class FinanceTracker:
                 if category not in self.categories:
                     self.categories.add(category)
                     self.total_expenses[category] = 0
+        except FileNotFoundError:
+            pass
+    
+    def save_total_expenses(self):
+        with open("total_expenses"+self.name+".json" ,"w") as file:
+            json.dump(self.total_expenses, file)
+    
+    def load_total_expenses(self):
+        try:
+            with open("total_expenses"+self.name+".json" ,"r") as file:
+                self.total_expenses = json.load(file)
         except FileNotFoundError:
             pass
